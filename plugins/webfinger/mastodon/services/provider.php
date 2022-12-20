fbdabaff2b29bb38e55e7de27c61ab0c639d3797
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-12-20 15:35:15 +0200

    Mastodon integration with WebFinger

---
+++ plugins/webfinger/mastodon/services/provider.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package   FediverseForJoomla
+ * @copyright Copyright (c)2022 Nicholas K. Dionysopoulos
+ * @license   https://opensource.org/licenses/GPL-3.0 GPL-3.0-or-later
+ */
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseDriver;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\WebFinger\Mastodon\Extension\Mastodon;
+
+return new class implements ServiceProviderInterface {
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 * @since   2.0.0
+	 */
+	public function register(Container $container)
+	{
+		$container->set(
+			PluginInterface::class,
+			function (Container $container) {
+				$pluginsParams = (array) PluginHelper::getPlugin(type: 'webfinger', plugin: 'mastodon');
+				$dispatcher    = $container->get(DispatcherInterface::class);
+				$plugin        = new Mastodon(subject: $dispatcher, config: $pluginsParams);
+
+				$plugin->setApplication(application: Factory::getApplication());
+				$plugin->setDatabase(db: $container->get(DatabaseDriver::class));
+
+				return $plugin;
+			}
+		);
+	}
+};
