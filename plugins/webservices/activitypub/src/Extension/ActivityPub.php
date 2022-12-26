9fa5b9bc2996a3bc16faed4e7f3905e44d17e7fd
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-12-26 17:06:56 +0200

    Remove unnecessary complexity

--- plugins/webservices/activitypub/src/Extension/ActivityPub.php
+++ plugins/webservices/activitypub/src/Extension/ActivityPub.php
@@ -15,8 +15,6 @@ use Joomla\CMS\Router\ApiRouter;
 use Joomla\Event\Event;
 use Joomla\Event\SubscriberInterface;
 use Joomla\Router\Route;
-use Negotiation\Exception\Exception;
-use Negotiation\Negotiator;
 
 class ActivityPub extends CMSPlugin implements SubscriberInterface
 {
@@ -31,12 +29,12 @@ class ActivityPub extends CMSPlugin implements SubscriberInterface
 	{
 		return [
 			'onBeforeApiRoute' => 'registerRoutes',
-			'onAfterApiRoute'  => 'fixBrokenJoomlaFormat',
+			'onAfterApiRoute'  => 'applyCustomJoomlaFormat',
 		];
 	}
 
 	/**
-	 * Workaround for Joomla bug #39495
+	 * I need to always use the `json` format in my component. This method makes it so.
 	 *
 	 * @param   Event  $e
 	 *
@@ -44,7 +42,7 @@ class ActivityPub extends CMSPlugin implements SubscriberInterface
 	 * @since   2.0.0
 	 * @see     https://github.com/joomla/joomla-cms/issues/39495
 	 */
-	public function fixBrokenJoomlaFormat(Event $e): void
+	public function applyCustomJoomlaFormat(Event $e): void
 	{
 		/** @var ApiApplication $app */
 		[$app] = $e->getArguments();
@@ -54,55 +52,14 @@ class ActivityPub extends CMSPlugin implements SubscriberInterface
 			return;
 		}
 
-		// Get the 'format' request parameter from the application's input
+		// We always use the plain old JSON format as we're handling the internals ourselves.
 		if ($app->input->getMethod() === 'POST')
 		{
-			$format = $app->input->post->get('format', '');
+			$app->input->post->set('format', 'json');
 		}
 		else
 		{
-			$format = $app->input->get('format', '');
-		}
-
-		if (is_array($format))
-		{
-			// At this point the format entries have lost all of their punctuation. Reshape 'em!
-			$format = array_map(
-				fn($f) => match ($f)
-				{
-					'applicationactivityjson' => 'application/activity+json',
-					'applicationldjson' => 'application/ld+json',
-					'applicationvnd.apijson' => 'application/vnd.api+json',
-					'applicationjson' => 'application/json',
-					default => $f
-				},
-				$format
-			);
-
-			// Re-run the Negotiator
-			try
-			{
-				$negotiator = new Negotiator();
-				$mediaType  = $negotiator->getBest($app->input->server->getString('HTTP_ACCEPT'), $format);
-				$format     = $mediaType->getValue();
-			}
-			catch (Exception $e)
-			{
-				// If an error occurred, fall back to Joomla API Application's default format
-				$format = 'application/vnd.api+json';
-			}
-		}
-
-		$format = $this->translateFormat($format);
-
-		// Set the format back to the application
-		if ($app->input->getMethod() === 'POST')
-		{
-			$app->input->post->set('format', $format);
-		}
-		else
-		{
-			$app->input->set('format', $format);
+			$app->input->set('format', 'json');
 		}
 	}
 
